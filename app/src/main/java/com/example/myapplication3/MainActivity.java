61b06d7afc171519d84b96a47e39999eae27bfaa
Anastasiia.Zakordonets <anastassia.zakordonets@gmail.com>
2019-06-08 16:38:31 +0200

    Two buttons

--- app/src/main/java/com/example/myapplication3/MainActivity.java
+++ app/src/main/java/com/example/myapplication3/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.myapplication3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void buttonPayClick(View v)
+    {
+        TextView tv = (TextView)findViewById(R.id.textView);
+        tv.setText("Payworks Pay");
+    }
+
+    public void buttonRefundClick(View v)
+    {
+        TextView tv = (TextView)findViewById(R.id.textView);
+        tv.setText("Payworks Refund");
     }
 }
